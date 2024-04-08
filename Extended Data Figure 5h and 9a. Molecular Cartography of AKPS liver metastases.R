5637c10df6627999f10630bb78ae1cb5ec149884
CocoBorrelli <56259073+CocoBorrelli@users.noreply.github.com>
2024-04-08 16:01:01 +0200

    Update Extended Data Figure 5h and 9a. Molecular Cartography of AKPS liver metastases.R

--- Extended Data Figure 5h and 9a. Molecular Cartography of AKPS liver metastases.R
+++ Extended Data Figure 5h and 9a. Molecular Cartography of AKPS liver metastases.R
@@ -5,14 +5,13 @@ library(Seurat)
 library(ggplot2)
 library(MetBrewer)
 
-
 #load the dataset
 Resolve_seurat_anno <- readRDS("Resolve_seurat_anno.rds")
 DimPlot(Resolve_seurat_anno)
 DimPlot(Resolve_seurat_anno, group.by = "samples")
 Idents(Resolve_seurat_anno) <- "samples"
 
-#split in metastatic livers and naive livers
+#split in metastatic livers and naive livers 
 no_mets <- subset(Resolve_seurat_anno, idents = "no_mets")
 mets <- subset(Resolve_seurat_anno, idents = "mets")
 
@@ -20,7 +19,7 @@ mets <- subset(Resolve_seurat_anno, idents = "mets")
 Idents(Resolve_seurat_anno) <- "annotation"
 hepatocytes <- subset(Resolve_seurat_anno, idents = c("Hepatocytes_CV", "Hepatocytes_PV"))
 
-#plot Plxnb2 expression in central vs portal hepatocytes 
+#plot Plxnb2 expression in central vs portal hepatocytes (Extended Data Fig 5g)
 VlnPlot(hepatocytes, features="Plxnb2", cols=met.brewer("Archambault", 2), pt.size = 0)+
   theme_classic() + 
   theme(text = element_text(size=20, colour = "black")) + RotatedAxis() + ylim(0.01,4)+
@@ -33,6 +32,6 @@ dePlxnb2 <- FindMarkers(hepatocytes, ident.1= "Hepatocytes_PV", ident.2="Hepatoc
 View(dePlxnb2)
 
 
-####plot Plxnb2 and class IV semaphorin expression in metastatic liver
+####plot Plxnb2 and class IV semaphorin expression in metastatic liver (Extended Data Fig 9a)
 VlnPlot(mets, features = c("Sema4a", "Sema4c", "Sema4d", "Sema4g", "Plxnb2"), cols = c("yellow", "red", "green", "violet", "blue"), stack = TRUE, sort = TRUE,  flip = TRUE) + ylim(0.001,7) +
   theme(legend.position = "none") +ggsave("Semaexpression.pdf", width=6, height = 5)
