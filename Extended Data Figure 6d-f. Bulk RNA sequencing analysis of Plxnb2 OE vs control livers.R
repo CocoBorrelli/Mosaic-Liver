3c66eccaa11170f304ba42f9d2e1c92060de2ab1
CocoBorrelli <56259073+CocoBorrelli@users.noreply.github.com>
2024-04-11 11:26:42 +0200

    Update Extended Data Figure 6d-f. Bulk RNA sequencing analysis of Plxnb2 OE vs control livers.R

--- Extended Data Figure 6d-f. Bulk RNA sequencing analysis of Plxnb2 OE vs control livers.R
+++ Extended Data Figure 6d-f. Bulk RNA sequencing analysis of Plxnb2 OE vs control livers.R
@@ -1,4 +1,4 @@
-#Code for the analysis of bulk RNA sequencing data of AKPS Sema4KD (quadruple sempahorin shRNA) vs emppty vector controls
+#Code for the analysis of bulk RNA sequencing data of Plxnb2 OE vs control (sgNT) livers
 
 #load packages
 library(rlang)
@@ -19,154 +19,80 @@ library(data.table)
 library(stringr)
 library(tidyr)
 library("edgeR")
+library(org.Mm.eg.db)
 
-######### ANNOTATE AND CREATE COUNT MATRICES #########
-#this function annotates the UMI count data with external gene names using Ensembl biomart for mouse species. The resulting dataframe contains gene expression data with annotated gene names.
-Annotation_mouse <- function(zUMI_output, prefix) {
-  ensembl<-useEnsembl(biomart="ensembl")
-  list<-listDatasets(ensembl)
-  mart <- useEnsembl(biomart="ensembl", dataset="mmusculus_gene_ensembl",version = 95)
-  attributes<-listAttributes(mart)
-  gene_ids <- getBM(attributes = c("ensembl_gene_id_version","external_gene_name"), mart = mart)
-  dataframe<-as.data.frame(as.matrix(zUMI_output$umicount$exon$all))%>%
-    as.matrix(.)
-  colnames(dataframe)<-paste(colnames(dataframe), prefix,sep = "_") 
-  dataframe<-mutate(as.data.frame(dataframe),ensembl_gene_id_version=rownames(dataframe))
-  join<-dataframe%>%
-    left_join(dplyr::select(gene_ids,1:2))
-  length(unique(join$external_gene_name))
-  join<-join[!duplicated(join$external_gene_name),]
-  join[is.na(join)]<-0 #make all empty value to zero
-  rownames(join)<-join$external_gene_name
-  join<-dplyr::select(join,-ensembl_gene_id_version,-external_gene_name)
-  return(join)
-}
+#import bulk counts 
+counts <- read.csv("bulk_counts.csv", row.names=1)
+colnames(counts)
+counts <-counts[-grep("__",rownames(counts)),]
+colnames(counts)
+
+#subset specific samples 
+
+AAV <- counts[,1:8]
+colnames(AAV)
+colnames(AAV) <- c("liver_OE_1","liver_OE_2", "liver_OE_3", "liver_KO_1", "liver_NT_1", "liver_KO_2", "liver_KO_3", "liver_NT_2")
 
 
-#read in dgecounts.rds file after zUMI demultiplexing, every pool contains multiple samples with individual barcodes introduced with the mcSCRB-seq protocol
-#barcode to sample assignment
-#barcode          sample               
-# "ACAGTG_poolA"  mets in AMO25713          
-#"AGTTCC_poolA"   AKPS sLPmCherry 3
-#"ATCACG_poolA"   KPC shRNA EV 1
-#"ATTCCT_poolA"   KPC parental
-# "CACTCA_poolA"  AKPS shRNA Sema4s 2       
-# "CTATAC_poolA"  KPC shRNA Sema4s 2
-#"GCCAAT_poolA"   mets in AMO25715          
-#"TACAGC_poolA"   KPC shRNA EV 2
-#"TATAAT_poolA"   KPC shRNA EV 3
-#"AGTCAA_poolB"   AKPS sLPmCherry 2
-#"CCAACA_poolB"   AKPS shRNA EV 3           
-#"CTAGCT_poolB"   KPC shRNA Sema4s 1
-#"GTAGAG_poolB"   AKPS shRNA EV 1           
-#"TAATCG_poolB"   AKPS shRNA Sema4s 1       
-#"TCATTC_poolB"   mets in AMO25297          
-#"TCGAAG_poolB"   AKPS shRNA Sema4s 3       
-# "TGACCA_poolB"  mets in AMO25712          
-# "TTAGGC_poolB"  mets in AMO25710          
-# "ATCACG_poolC"  mets in AMO25294          
-# "ATGAGC_poolC"  AKPS shRNA EV 2           
-#"CAGATC_poolC"   mets in AMO25856          
-#"CCGTCC_poolC"   mets derived org line 1
-#"GACGAC_poolC"   mets derived org line 2
-#"GAGTGG_poolC"   mets in AMO25934          
-#"GTGAAA_poolC"   AKPS sLPmCherry 1
-
-poolA <- readRDS("poolA/zUMIs_output/expressionbulk_orgs_round2.dgecounts.rds")
-colnames(poolA$umicount$exon$all)
-poolA_ann <- Annotation_mouse(poolA, "poolA")
-colnames(poolA_ann) <- c("mets in AMO25713", "AKPS sLPmCherry 3","KPC parental", 
-                         "AKPS shRNA Sema4s 2", "KPC shRNA EV 1", "KPC shRNA Sema4s 2", 
-                         "mets in AMO25715", "KPC shRNA EV 2", "KPC shRNA EV 3")
-poolA_ann$Gene <- rownames(poolA_ann)
-
-poolB <- readRDS("poolB/zUMIs_output/expression/bulk_orgs_round2.dgecounts.rds")
-colnames(poolB$umicount$exon$all)
-poolB_ann <- Annotation_mouse(poolB, "poolB")
-colnames(poolB_ann) <- c("AKPS sLPmCherry 2", 
-                         "AKPS shRNA EV 3", 
-                         "KPC shRNA Sema4s 1",
-                         "AKPS shRNA EV 1", 
-                         "AKPS shRNA Sema4s 1", 
-                         "mets in AMO25297", 
-                         "AKPS shRNA Sema4s 3", 
-                         "mets in AMO25712", 
-                         "mets in AMO25710")      
-poolB_ann$Gene <- rownames(poolB_ann) 
-
-poolC <- readRDS("poolC/zUMIs_output/expression/bulk_orgs_round2.dgecounts.rds")
-colnames(poolC$umicount$exon$all)
-poolC_ann <- Annotation_mouse(poolC, "poolC")
-View(poolC_ann)
-colnames(poolC_ann) <- c("mets in AMO25294" , "AKPS shRNA EV 2", "mets in AMO25856",
-                                        "mets derived org line 1",  "mets derived org line 2",
-                                        "mets in AMO25934" ,"AKPS sLPmCherry 1")
-poolC_ann$Gene <- rownames(poolC_ann)
-
-
-#create sample count matrix
-poolAB <- merge(poolA_ann, poolB_ann,by="Gene",  stringsAsFactors = TRUE)
-colnames(poolAB)
-poolABC <- merge(poolAB, poolC_ann,by="Gene",  stringsAsFactors = TRUE)
-colnames(poolABC)
-
-all_samples_mat <- as.matrix(poolABC[2:26])
-rownames(all_samples_mat)<- poolABC$Gene
-
-# Subset data to include specific columns
-AAV <- all_samples_mat[,c(2,8,16,18,19,20,22,25)]
-colnames(AAV) <- c("AMO25713", "AMO25715", "AMO25297", "AMO25712", "AMO25710", "AMO25294", "AMO25856", "AMO25934")
-
-
+######### DIFFERENTIAL GENE EXPRESSION ANALYSIS WITH edgeR ##########
 #Create DGEList object
-group <- factor(c("Plxnb2_OE","Plxnb2_OE","sgNT","Plxnb2_OE","sgNT","Plxnb2_OE","sgNT")) #1 is NT 2 is OE
+group <- factor(c("liver_OE","liver_OE", "liver_OE", "liver_KO", "liver_NT", "liver_KO", "liver_KO", "liver_NT")) 
 y <- DGEList(counts=AAV,group=group)
 y$samples
-# The las argument rotates the axis names
 barplot(y$samples$lib.size,names=colnames(y), las=2, main = "Barplot of library sizes")
 
-# Filter reads by counts: Most of the samples should have at least 10 reads, normalize the library and estimate dispersion
-#keep <- filterByExpr(y, min.count = 10)
+# Filter reads by counts
 keep <- filterByExpr(y)
 y <- y[keep, , keep.lib.sizes=FALSE]
 y <- calcNormFactors(y)
 y$samples
-design <- model.matrix(~group)
-#y <- estimateDisp(y)
-y <- estimateDisp(y,design)
-plotMDS(y)
+
+# Create a design matrix for the analysis
+design <- model.matrix(~0 + group)
+colnames(design) <- levels(group)
+y <- estimateDisp(y,design, robust=T)
+y$common.dispersion
+plotMDS(y, top= 500)
+
+# Calculate log2 counts per million (logCPM) and view the results
 logcpm <- cpm(y, log=TRUE)
 View(logcpm)
-#write.table(logcpm, file="logcpm_OE_NT.txt")
+write.table(logcpm, file="logcpm_OE_NT.txt")
 
-
-design <- model.matrix(~0 +group)
-colnames(design) <- levels(group)
-design
-fit <- glmQLFit(y, design, robust=T)
+# Fit a quasi-likelihood negative binomial generalized linear model (GLM) with robust estimation
+fit <- glmQLFit(y, design, robust=T, adjust.method ="fdr")
 head(fit$coefficients)
 plotQLDisp(fit)
 
-con<-makeContrasts(Plxnb2_OE - sgNT,levels=design)
-qlf_liver<-glmQLFTest(fit,contrast=con)
-View(qlf_liver$table)
-tr<-glmTreat(fit,contrast=con,lfc=log2(1))
+# Define the contrast for the differential expression analysis
+con<-makeContrasts(liver_OE - liver_NT,levels=design)
+
+# Perform quasi-likelihood F-test for the defined contrast
+qlf_OE<-glmQLFTest(fit,contrast=con)
+
+#annotate with external gene names
+qlf_OE$table$genes <- mapIds(org.Mm.eg.db, 
+                                keys=gsub("\\.[0-9]{1,2}","",rownames(qlf_OE)),
+                                keytype = "ENSEMBL", column="SYMBOL", 
+                                multiVals = "first")
+
+View(qlf_OE$table)
+tr<-glmTreat(fit,contrast=con,lfc=log2(1.5))
 summary(decideTests(tr))
 
 # Extract corrected DEGs using False Discovery Rate (FDR) adjustment
-corrected_DEGs <- na.omit(topTags(qlf_liver, n = Inf, adjust.method = "fdr", sort.by = "PValue", p.value = 1)$table)
-View(corrected_DEGs)
+corrected_DEGs_OE <- topTags(qlf_OE, n = Inf, adjust.method = "fdr", sort.by = "PValue", p.value = 1)$table
+View(corrected_DEGs_OE)
 
 
 ##generate volcano plot
 #this function generates a volcano plot to visualize differentially expressed genes (DEGs) based on specified thresholds for False Discovery Rate (FDR) and absolute log2 fold change. 
 #It marks DEGs meeting the thresholds as 'Significant' and assigns gene names to them. The volcano plot is customized with colors, labels, axis limits, and title. The function returns the generated volcano plot.
-
 DEGs_volcano <- function(DEGs, p_treshold, FC_treshold, title, color, upperylim, xlim) {
   DEGs$Significant <- ifelse((DEGs$FDR < p_treshold & abs(DEGs$logFC) > FC_treshold ), "Significant", "Not Sig")
-  DEGs$gene <- ifelse((DEGs$FDR < p_treshold & abs(DEGs$logFC) > FC_treshold ), rownames(DEGs), NA)
+  DEGs$genes <- ifelse((DEGs$FDR < p_treshold & abs(DEGs$logFC) > FC_treshold ), DEGs$genes, NA)
   
-  plot <-  ggplot(data=DEGs, aes(x=logFC, y=-log10(FDR), fill=factor(Significant), label = DEGs$gene) ) + 
+  plot <-  ggplot(data=DEGs, aes(x=logFC, y=-log10(FDR), fill=factor(Significant), label = DEGs$genes) ) + 
     theme_bw()+ 
     theme( panel.grid.major.x = element_blank(),
            panel.grid.minor.x = element_blank(),
@@ -174,7 +100,7 @@ DEGs_volcano <- function(DEGs, p_treshold, FC_treshold, title, color, upperylim,
            panel.grid.minor.y = element_blank())+
     geom_point(shape = 21,color=color)+
     scale_fill_manual(values = c(color, "black")) +
-    geom_text_repel(size=4, max.overlaps = Inf)+
+    geom_text_repel(size=3, max.overlaps = Inf)+
     xlab("log2 fold change") + ylim(0,upperylim)+xlim(-xlim,xlim)+
     ylab("-log10(FDR)") +  labs(title = title)+
     theme(legend.position = "none", text = element_text(size=12),
@@ -183,8 +109,7 @@ DEGs_volcano <- function(DEGs, p_treshold, FC_treshold, title, color, upperylim,
   return(plot)
 }
 
-corrected_DEGs$gene <- rownames(corrected_DEGs)
-DEGs_volcano(corrected_DEGs, 0.2, 3, "Plxnb2 vs NT", "grey",0.8, 8) #+ggsave("mets_volcano.pdf", width =8,  height=8)
+DEGs_volcano(corrected_DEGs_OE, 0.5, 6, "Plxnb2-OE vs NT", "grey",1.7, 10)+ggsave("Plxnb2OEvsNT_volcano.pdf", width =7,  height=5)
 
 ########## GENE SET ENRICHMENT ANALYSIS #############
 #import datasets
@@ -201,11 +126,11 @@ preranked_BP <- function(x) {
     na.omit()%>%
     mutate(ranking=logFC)
   ranks <- ranks$ranking
-  names(ranks) <- rownames(x)
+  names(ranks) <- x$genes%>% na.omit()
   head(ranks, 10)
   
   BP_x <- fgsea(pathways = BP, 
-                stats = ranks,
+                stats = ranks%>% na.omit(),
                 minSize=10,
                 maxSize=500,
                 nperm=1000000)
@@ -215,17 +140,57 @@ preranked_BP <- function(x) {
   return(BP_x)
 }
 
-BP_enrichment <- preranked_BP(qlf_liver$table)
-View(BP_enrichment)
+BP_enrichment_OE <- preranked_BP(DEGs_OE)
+View(BP_enrichment_OE)
 
-#plot
-ggplot(BP_enrichment %>% filter(abs(NES)>2.1 & pval<0.01) %>% head(n= 100), aes(reorder(pathway, NES), NES)) +
-  geom_point(aes(size=size, colour=pval)) +
+ggplot(BP_enrichment_OE %>% filter(abs(NES)>1 & padj<0.05) %>% head(n= 100), aes(reorder(pathway, NES), NES)) +
+  geom_point(aes(size=size, colour=padj)) +
   scale_color_gradientn(colours = brewer.pal(11,"RdYlBu")[1:11]) +
   coord_flip() +
   labs(x="Pathway", y="Normalized Enrichment Score",
        title=" ") + 
   theme_classic()+
   labs(y="")+   
-  theme(axis.text.y=element_text(size=10)) + ggsave("mets_BP.pdf", width =10,  height=8)
+  theme(axis.text.y=element_text(size=10)) + ggsave("PLxnb2OEvsNT_BP.pdf", width =7,  height=5)
 
+
+
+########CELL TYPE ENRICHMENT WITH ENRICHR########
+#save DEGs
+write.csv(na.omit(corrected_DEGs_OE), file="DEGS_Plxnb2OEvsNT.csv") #this list of genes was used in EnrichR to estimate cell type composition
+
+#import saved results
+Enrichr_celltypedeconTM_downinOE <- read.delim("/media/Coco/MOSAIC LIVER/Experiments/bulk_liver_and_lungs/Enrichr_celltypedeconTM_downinOE.txt")
+head(Enrichr_celltypedeconTM_downinOE)
+Enrichr_celltypedeconTM_upinOE <- read.delim("/media/Coco/MOSAIC LIVER/Experiments/bulk_liver_and_lungs/Enrichr_celltypedeconTM_upinOE.txt")
+head(Enrichr_celltypedeconTM_upinOE)
+
+# Select columns of interest from Enrichr_celltypedeconTM_downinOE dataframe
+down <- Enrichr_celltypedeconTM_downinOE[, c(1,4,7)]
+
+# Add a new column "group" with value "DOWN" to the down dataframe
+down$group <- "DOWN"
+
+# Invert the values of the "Odds.Ratio" column in the down dataframe
+down$Odds.Ratio <- -down$Odds.Ratio
+
+# Select columns of interest from Enrichr_celltypedeconTM_upinOE dataframe
+up <- Enrichr_celltypedeconTM_upinOE[, c(1,4,7)]
+
+# Add a new column "group" with value "UP" to the up dataframe
+up$group <- "UP"
+
+# Bind the down and up dataframes row-wise to create a new dataframe "plot"
+plot <- rbind(down, up)
+
+# Plotting the data using ggplot2 library
+ggplot(data=plot, aes(x=plot$Odds.Ratio, y=-log10(plot$Adjusted.P.value), fill=factor(group)) ) + 
+    theme_bw()+ 
+    theme( panel.grid.major.x = element_blank(),
+           panel.grid.minor.x = element_blank(),
+           panel.grid.major.y = element_blank(),
+           panel.grid.minor.y = element_blank())+
+    geom_point() +
+    ylim(0,1.5)+ xlim(-4,4)+
+    geom_hline(yintercept = -log10(0.05), linetype="dashed",  color = "black", size=0.1)+
+    geom_vline(xintercept = 0, linetype="dashed",  color = "black", size=0.1) +ggsave("Plxnb2OEvsNT_celltype.pdf", width =8,  height=6)
